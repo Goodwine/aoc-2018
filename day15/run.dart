a39c2280fdcfdb1f11f2aa805cec0bc7293e3846
Carlos Israel Ortiz García <2022649+Goodwine@users.noreply.github.com>
2022-06-07 03:27:40 -0700

    2018 day 15 - still not there
    I messed up the 'move' part and somehow the 'medium' case gets
    lost on movement 24.
    
    Then I need to figure out how to avoid doing extra work when exploring the maze
    I realize I can reuse work or I could do some A* but it won't be enough
    I wonder if I should fix it, time it and consider just letting it run for several hours
    
    🤔

--- day15/run.dart
+++ day15/run.dart
@@ -1,15 +1,27 @@
+import 'dart:io';
 import 'dart:math';
 
 import 'package:aoc2018/aoc.dart';
-import 'package:tuple/tuple.dart';
 
 void main() {
-  solve(process, part1, part2);
+  solve(process, part1, part2, extra: "medium");
 }
 
 Future<List<List<int>>> process(String path) async {
-  var data = await read(path);
-  return data.map((line) => line.codeUnits.map((e) => e).toList()).toList();
+  var data = (await read(path)).map((line) => [...line.codeUnits]).toList();
+  if (data.length == data[0].length) return data;
+  if (data.length < data[0].length) {
+    return [
+      ...data,
+      ...List.filled(data[0].length - data.length, List.filled(data[0].length, pound)),
+    ];
+  }
+  return data
+      .map((line) => [
+            ...line,
+            ...List.filled(data.length - data[0].length, pound),
+          ])
+      .toList();
 }
 
 var code = "#.GE".codeUnits;
@@ -18,8 +30,10 @@ var space = code[1];
 var goblin = code[2];
 var elf = code[3];
 
-int part1(List<List<int>> data) {
-  final allUnits = data
+int part1(List<List<int>> area) {
+  assert(area.length == area[0].length);
+
+  final allUnits = area
       .asMap()
       .entries
       .expand((line) => line.value
@@ -29,19 +43,18 @@ int part1(List<List<int>> data) {
           .map((e) => Unit(Point(e.key, line.key, e.value))))
       .toList();
 
-  var turns = combat(data, allUnits);
+  var turns = combat(area, allUnits);
   var allHP = allUnits.map((e) => e.hp).where((e) => e > 0).reduce((acc, v) => acc + v);
 
   return turns * allHP;
 }
 
-int combat(List<List<int>> data, List<Unit> allUnits) {
-  final maxSize = max(data.length, data[0].length);
-
+int combat(List<List<int>> area, List<Unit> allUnits) {
   var turns = 0;
   while (true) {
+    printArea(turns, area, allUnits);
     for (final unit in allUnits) {
-      if (!turn(data, maxSize, allUnits, unit)) return turns;
+      if (!turn(area, allUnits, unit)) return turns;
     }
     turns++;
   }
@@ -66,25 +79,49 @@ class Unit {
 
   @override
   int get hashCode => point.hashCode;
+  @override
+  String toString() => "$point,hp:$hp,dmg:$dmg";
 
   void move(Direction dir) => point = point.move(dir);
   Iterable<Point> next(int maxSize) => point.next(maxSize);
 }
 
-bool turn(List<List<int>> area, int maxSize, List<Unit> allUnits, Unit unit) {
-  var targets = allUnits.where((e) => e != unit).where((e) => e.hp > 0).toList();
+void printArea(int turn, List<List<int>> area, List<Unit> allUnits) {
+  print("TURN: $turn");
+  allUnits.forEach(print);
+  var emptyArea = area
+      .map(String.fromCharCodes)
+      .map((line) => line.replaceAll("G", ".").replaceAll("E", ".").codeUnits)
+      .map((line) => [...line])
+      .toList();
+
+  for (final unit in allUnits.where((element) => element.hp > 0)) {
+    emptyArea[unit.point.y][unit.point.x] = unit.point.id;
+  }
+  emptyArea.map(String.fromCharCodes).forEach(print);
+
+  // stdin.readLineSync();
+}
+
+bool turn(List<List<int>> area, List<Unit> allUnits, Unit unit) {
+  if (unit.hp < 0) return true; // turn ends - unit is dead
+  var liveUnits = allUnits.where((e) => e.hp > 0).toList();
+  var targets = liveUnits.where((e) => e.point.id != unit.point.id).toList();
+  var allies = liveUnits.where((e) => e.point.id == unit.point.id).toList();
   if (targets.isEmpty) return false; // combat ends - no more targets
 
-  var closest = closestTarget(area, unit, targets);
+  var closest = closestTarget(area, unit, targets, allies);
   if (closest == null) return true; // turn ends - no target was reachable
   if (closest.path.length > 1) {
     unit.move(closest.path.first);
   }
 
   var possibleAttack =
-      unit.next(maxSize).expand((e) => targets.where((target) => target.point == e));
-  var maxHP = possibleAttack.map((e) => e.hp).reduce(max);
-  var attack = possibleAttack.firstWhere((e) => e.hp == maxHP);
+      unit.next(area.length).expand((e) => targets.where((target) => target.point == e)).toList();
+  if (possibleAttack.isEmpty) return true; // turn ends - nothing to attack
+
+  var minHP = possibleAttack.map((e) => e.hp).reduce(min);
+  var attack = possibleAttack.firstWhere((e) => e.hp == minHP);
   attack.hp -= unit.dmg;
 
   return true; // turn ends - keep moving
@@ -92,9 +129,38 @@ bool turn(List<List<int>> area, int maxSize, List<Unit> allUnits, Unit unit) {
 
 class PathTarget {
   final List<Direction> path;
-  final Point target;
+  final Point point;
 
-  PathTarget(this.path, this.target);
+  PathTarget(this.path, this.point);
 }
 
-PathTarget? closestTarget(List<List<int>> area, Unit unit, List<Unit> targets) {}
+// bfs
+PathTarget? closestTarget(List<List<int>> area, Unit unit, List<Unit> targets, List<Unit> allies) {
+  final queue = [PathTarget([], unit.point)];
+  final visited = allies.map((a) => a.point).toSet();
+  final targetSet = targets.map((t) => t.point).toSet();
+
+  while (queue.isNotEmpty) {
+    final current = queue.removeAt(0);
+    visited.add(current.point);
+
+    var validDirections = directions.where((d) {
+      final p = current.point.move(d);
+      return p.x >= 0 &&
+          p.y >= 0 &&
+          p.x < area.length &&
+          p.y < area.length &&
+          !visited.contains(p) &&
+          !{pound}.contains(area[p.y][p.x]);
+    }).toList();
+    var next = validDirections
+        .map((d) => PathTarget([...current.path, d], current.point.move(d)))
+        .toList();
+
+    var maybe = next.where((n) => targetSet.contains(n.point));
+    if (maybe.isNotEmpty) return maybe.first;
+
+    queue.addAll(next);
+  }
+  return null;
+}
