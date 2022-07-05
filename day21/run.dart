c1481b7373ba291ac875be8071ab361bfec31d5f
Carlos Israel Ortiz García <2022649+Goodwine@users.noreply.github.com>
2022-07-05 14:06:05 -0700

    day 21 semi parsed

---
+++ day21/run.dart
@@ -0,0 +1,21 @@
+import 'package:aoc2018/aoc.dart';
+
+void main() {
+  solve(readInts, part1, part2, skipSmall: true);
+}
+
+int part1(Iterable<int> data) {
+  return data.reduce((acc, v) => acc + v);
+}
+
+int part2(Iterable<int> data) {
+  var list = data.toList();
+  var seen = {0};
+  var loc = 0;
+  var idx = 0;
+  while (true) {
+    loc += list[idx];
+    if (!seen.add(loc)) return loc;
+    idx = (idx + 1) % data.length;
+  }
+}
