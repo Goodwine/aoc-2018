14c320f275c5b495e070689754d069c928c5d665
Carlos Israel Ortiz García <2022649+Goodwine@users.noreply.github.com>
2022-07-07 03:06:15 -0700

    2018 day 21
    first I had a bug in code as documented in converted.txt
    then I was naively trying ALL possible numbers.. which actually worked but was very slow
    I had to use Isolate and it would process all possibilities within the range
    but I probably had my counters wrong because I was getting WAs
    
    after realizing (looking online) that I don't have to test all numbers
    but instead capture all different values generated to r[4] I was able to get the result

--- day21/run.dart
+++ day21/run.dart
@@ -1,21 +1,29 @@
 import 'package:aoc2018/aoc.dart';
 
 void main() {
-  solve(readInts, part1, part2, skipSmall: true);
+  solve((_) async => 0, part1, part2, skipSmall: true);
 }
 
-int part1(Iterable<int> data) {
-  return data.reduce((acc, v) => acc + v);
-}
+const jobs = 20;
 
-int part2(Iterable<int> data) {
-  var list = data.toList();
+int part1(int _) => input(1);
+int part2(int _) => input(16777216);
+
+int input(int maxLoops) {
+  var a = 0;
+  var b = 0;
   var seen = {0};
-  var loc = 0;
-  var idx = 0;
-  while (true) {
-    loc += list[idx];
-    if (!seen.add(loc)) return loc;
-    idx = (idx + 1) % data.length;
+  var last = 0;
+
+  for (var i = 0; i < maxLoops; i++) {
+    a = b | 65536; // 2^16
+    b = 2024736;
+    while (true) {
+      b = (((b + a % 256) % 16777216) * 65899) % 16777216;
+      if (a < 256) break;
+      a = a ~/ 256;
+    }
+    if (seen.add(b)) last = b;
   }
+  return last;
 }
