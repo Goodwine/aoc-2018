32451e6ee80e6de7d3186868b8ab877d3de61efc
Carlos Israel Ortiz García <2022649+Goodwine@users.noreply.github.com>
2022-07-09 12:19:19 -0700

    2018 day 22 part 2
    solution is recursive DFS but fails due to StackOverflow and I have no idea how to increase stack-size limit

--- day22/run.dart
+++ day22/run.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:aoc2018/aoc.dart';
 import 'package:tuple/tuple.dart';
 
@@ -18,14 +20,14 @@ const modulo = 20183;
 const xIncrement = 16807;
 const yIncrement = 48271;
 
-int part1(Tuple2<int, Point> data) =>
-    gridify(data.item2, data.item1).expand((e) => e).reduce((acc, v) => acc + v);
+int part1(Tuple2<int, Point> data) => gridify(data.item2, data.item1)
+    .expand((e) => e)
+    .map((e) => e.index)
+    .reduce((acc, v) => acc + v);
 
 int erosion(int v, int d) => ((v % modulo) + d) % modulo;
 
-int part2(Tuple2<int, Point> data) => 0;
-
-List<List<int>> gridify(Point target, int depth) {
+List<List<Tile>> gridify(Point target, int depth) {
   var grid = List.generate(target.y + 1, (_) => List.filled(target.x + 1, 0));
 
   for (int y = 0; y < grid.length; y++) {
@@ -41,6 +43,70 @@ List<List<int>> gridify(Point target, int depth) {
   grid.last.last = 0; // 🤔 🤷🏽‍♂️
 
   return [
-    for (var row in grid) [for (var cell in row) cell % 3]
+    for (var row in grid) [for (var cell in row) Tile.values[cell % 3]]
   ];
+}
+
+int part2(Tuple2<int, Point> data) {
+  var maxSize = Point(data.item2.x + 10, data.item2.y + 10);
+  var grid = gridify(maxSize, data.item1);
+  grid[data.item2.y][data.item2.x] = Tile.rocky;
+
+  return DP(data.item2, maxSize, grid).navigate(Point(0, 0), Tool.gear, 1);
+}
+
+enum Tile { rocky, wet, narrow }
+
+enum Tool { gear, torch, none }
+
+const allowed = {
+  Tile.rocky: {Tool.gear, Tool.torch},
+  Tile.wet: {Tool.gear, Tool.none},
+  Tile.narrow: {Tool.torch, Tool.none},
+};
+
+typedef DPKey = Tuple2<Point, Tool>;
+
+class DP {
+  List<List<Tile>> grid;
+  Point target;
+  Point maxSize;
+  Map<DPKey, int> dp = {};
+
+  DP(this.target, this.maxSize, this.grid);
+
+  int navigate(Point p, Tool t, int time) {
+    var toolSet = allowed[grid[p.y][p.x]]!;
+    if (!toolSet.contains(t)) {
+      return 1 << 62; // infinite-ish
+    }
+    if (p == target) {
+      return time + (t == Tool.torch ? 1 : 7);
+    }
+    var dpk = Tuple2(p, t);
+    if (dp[dpk] != null && time >= dp[dpk]!) {
+      return 1 << 62; // infinite-ish
+    }
+    dp[dpk] = time;
+
+    var theOtherTool = toolSet.where((v) => v != t).first;
+
+    // Had to split because I am getting StackOverflow
+    var next = directions
+        .map(p.move)
+        .where((n) => n.x >= 0)
+        .where((n) => n.y >= 0)
+        .where((n) => n.x <= maxSize.x)
+        .where((n) => n.y <= maxSize.y)
+        .toList();
+
+    var currMin = 1 << 62; // infinite-ish
+    for (var n in next) {
+      // Had to split because I am getting StackOverflow
+      var sameTool = navigate(n, t, time + 1);
+      var otherTool = navigate(n, theOtherTool, time + 8);
+      currMin = [sameTool, otherTool, currMin].reduce(min);
+    }
+    return currMin;
+  }
 }
