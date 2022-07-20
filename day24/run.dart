ba38854ce51a7228413d704ad5a5d8c04913e5c7
Carlos Israel Ortiz García <2022649+Goodwine@users.noreply.github.com>
2022-07-19 23:54:51 -0700

    2018 day 24 part 1

--- day24/run.dart
+++ day24/run.dart
@@ -2,28 +2,74 @@ import 'package:aoc2018/aoc.dart';
 import 'package:tuple/tuple.dart';
 
 void main() {
-  solve((path) async {
-    var lines = await read(path);
-    var immune = lines.takeWhile((value) => value.isNotEmpty).skip(1).map(UnitGroup.from).toList();
-    var infection = lines.sublist(immune.length + 3).map(UnitGroup.from).toList();
-    return Tuple2(immune, infection);
-  }, part1, part2);
+  solve(
+    (path) async {
+      var lines = await read(path);
+      var immune = lines
+          .takeWhile((value) => value.isNotEmpty)
+          .skip(1)
+          .map((e) => UnitGroup.from(e, "immune"))
+          .toList();
+      var infection =
+          lines.sublist(immune.length + 3).map((e) => UnitGroup.from(e, "infection")).toList();
+      return Tuple2(immune, infection);
+    },
+    part1,
+    part2,
+  );
 }
 
 int part1(Tuple2<List<UnitGroup>, List<UnitGroup>> data) {
-  print(data);
-  return 0;
+  var immune = {...data.item1};
+  var infection = {...data.item2};
+
+  while (immune.isNotEmpty && infection.isNotEmpty) {
+    // target selection
+    var everything = [...immune, ...infection]..sort();
+    var chosen = <UnitGroup, UnitGroup>{}; // target: aggresor
+    for (var ug in everything) {
+      var enemies = ug.kind == "immune" ? infection : immune;
+      var possibleEnemies =
+          enemies.where((e) => !chosen.containsKey(e)).where((e) => ug.dmgTo(e) > 0);
+      if (possibleEnemies.isEmpty) continue;
+      var target = ug.chooseTarget(possibleEnemies);
+      chosen.putIfAbsent(target, () => ug);
+    }
+
+    // attack phase
+    var attacks = chosen.entries.toList()
+      ..sort((a, b) => b.value.initiative - a.value.initiative); // DESC initiative
+    for (var attack in attacks) {
+      var aggressor = attack.value;
+      var target = attack.key;
+      if (aggressor.size <= 0) continue;
+
+      target.size -= aggressor.dmgTo(target) ~/ target.hp; // attack
+
+      if (target.size <= 0) {
+        immune.remove(target);
+        infection.remove(target);
+      }
+    }
+  }
+
+  return [...immune, ...infection].map((e) => e.size).reduce((a, b) => a + b);
 }
 
 int part2(Tuple2<List<UnitGroup>, List<UnitGroup>> data) {
   return 0;
 }
 
-class UnitGroup {
-  late int size, hp;
-  late final dmg, initiative;
+class UnitGroup implements Comparable<UnitGroup> {
+  late int size;
+  late final int hp, dmg, initiative;
   late final String dmgType;
   final Set<String> weaknesses = {}, immunities = {};
+  final String kind, line;
+
+  int get effectiveDmg => dmg * size;
+  bool operator ==(Object b) => b is UnitGroup && b.kind == kind && b.line == line;
+  int get hashCode => Object.hash(kind, line);
 
   /// Parses single line like this:
   ///
@@ -32,7 +78,7 @@ class UnitGroup {
   ///
   /// with an attack that does 73 bludgeoning damage at initiative 6
   /// N-11 N-10 N-9  N-8  N-7  N-6 N-5        N-4    N-3 N-2       N-1
-  UnitGroup.from(String line) {
+  UnitGroup.from(this.line, this.kind) {
     var parts = line.split(" ");
 
     size = int.parse(parts[0]);
@@ -64,7 +110,30 @@ class UnitGroup {
 
   @override
   String toString() =>
-      "$size units each with $hp hit points (weak to ${weaknesses}; immune to ${immunities}) with an attack that does $dmg radiation damage at initiative $initiative\n";
+      "[$kind] $size units each with $hp hit points (weak to ${weaknesses}; immune to ${immunities}) with an attack that does $dmg radiation damage at initiative $initiative\n";
+
+  @override
+  int compareTo(UnitGroup b) {
+    var ed = b.effectiveDmg - effectiveDmg;
+    return ed == 0 ? b.initiative - initiative : ed;
+  }
+
+  UnitGroup chooseTarget(Iterable<UnitGroup> enemies) => (enemies.toList()
+        ..sort((a, b) {
+          var dmgToA = dmgTo(a);
+          var dmgToB = dmgTo(b);
+          if (dmgToA != dmgToB) return dmgToB - dmgToA; // DESC damage dealt
+          var ed = b.effectiveDmg - a.effectiveDmg; // DESC enemy effective dmg
+          if (ed != 0) return ed;
+          return b.initiative - a.initiative; // DESC enemy initiative
+        }))
+      .first;
+
+  int dmgTo(UnitGroup target) {
+    if (target.immunities.contains(dmgType)) return 0;
+    if (target.weaknesses.contains(dmgType)) return effectiveDmg * 2;
+    return effectiveDmg;
+  }
 }
 
 /// immune to fire, slashing
